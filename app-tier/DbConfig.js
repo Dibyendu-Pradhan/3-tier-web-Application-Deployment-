39b86ddb72b9dbf9f47a9871fed4728956f89b92
Dibyendu-Pradhan <153085616+Dibyendu-Pradhan@users.noreply.github.com>
2025-04-27 02:01:59 +0530

    Add files via upload

---
+++ app-tier/DbConfig.js
@@ -0,0 +1,6 @@
+module.exports = Object.freeze({
+    DB_HOST : 'database-1-instance-1.c1kyugme086t.ap-south-1.rds.amazonaws.com',
+    DB_USER : 'dp6654',
+    DB_PWD : 'kungfupan',
+    DB_DATABASE : 'database-1'
+});
